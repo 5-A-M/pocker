a315055b6c523d89003fe699e794f105d87ecc2a
datispham <datistpham@gmail.com>
2022-09-08 14:02:09 +0700

    third commit

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017',
-	'options': {
-		//'user':   'kvcpro',
-		//'pass':   'Minhhoang2009@',
-		'dbName': 'RVIP', // red
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex':       false,
-	},
+  url: "mongodb+srv://giang:giangvippro@cluster0.pffyt.mongodb.net/${database}?retryWrites=true&w=majority",
+  options: {
+    //'user':   'kvcpro',
+    //'pass':   'Minhhoang2009@',
+    dbName: "RVIP", // red
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //'autoIndex':       false,
+  },
 };
